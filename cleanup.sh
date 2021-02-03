08d9088ca3ce69c80b394bc9ebca0b426bb49e4a
Mike Ensor <ensor@google.com>
2021-02-03 21:59:18 +0000

    Adding license headers

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # This file will remove all resources built with this blueprint
 
@@ -12,6 +25,7 @@ while getopts 'q ': option
 do
     case "${option}" in
         q) QUIET="true";;
+        *) echo "INVALID Parameter"
     esac
 done
 
@@ -27,6 +41,7 @@ verify_cli_tools "${REQUIRED}"
 
 # Verify state bucket
 echo -e "${FANCY_NONE} Verifying access to Terraform Remote State Bucket"
+# shellcheck disable=SC2034
 BUCKET=$(gsutil ls -al gs://${TF_STATE_BUCKET} > /dev/null)
 if [ $? -gt 0 ]; then
     echo -e "${FANCY_FAIL} Access to Terraform State Bucket failed: $(gsutil ls -al gs://${TF_STATE_BUCKET})"
