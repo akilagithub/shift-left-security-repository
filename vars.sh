567a06d4b203d297fc2877cd0aa5bcb01c979cc8
Mike Ensor <mike@ensor.cc>
2021-02-11 13:58:17 -0800

    [skip ci] providing default environment variables for provision script to avoid code modifications

--- vars.sh
+++ vars.sh
@@ -15,7 +15,7 @@
 
 # Google project ID number to deploy resources into
 # 3 ways to set this variable:
-#   1. Set an environment variable called "PROJECT_ID"
+#   1. Create an environment variable called "PROJECT_ID" with the value of the project-id (no code changes needed)
 #   2. Use the current gcloud configuration's value (default if PROJECT_ID is not set)
 #   3. Replace the value with your google project in quotes
 export GOOGLE_PROJECT_ID="${PROJECT_ID:-$(gcloud config list --format 'value(core.project)')}"
@@ -23,7 +23,7 @@ export GOOGLE_PROJECT_ID="${PROJECT_ID:-$(gcloud config list --format 'value(cor
 # Name of the GCS bucket for the terraform state. Changing this is optional, by default a
 # bucket will be created using the <project-id>-tf-state format within the project.
 # 2 ways to change:
-#   1. Set an environment variable "TF_STATE_BUCKET" to the name of the bucket
+#   1. Set an environment variable "TF_STATE_BUCKET" to the name of the bucket (no code changes needed)
 #   2. Replace the value with your bucket name in quotes
 export TF_STATE_BUCKET="${TF_STATE_BUCKET:-${GOOGLE_PROJECT_ID}-tf-state}"
 
@@ -33,4 +33,7 @@ export TF_STATE_BUCKET="${TF_STATE_BUCKET:-${GOOGLE_PROJECT_ID}-tf-state}"
 # during the provision.sh file will be conducted with that GSA. 'TRUE' is the ONLY recognized value
 #
 # "FALSE" (default) will use the current user logged into gcloud.
-export CREATE_INFRA_GSA="FALSE"
+# 2 ways to change:
+#   1. Create an environment variable CREATE_INFRA_GSA with the intended value before running this script (no code changes needed)
+#   2. Replace the value below with the intended value
+export CREATE_INFRA_GSA="${CREATE_INFRA_GSA}:-FALSE"
